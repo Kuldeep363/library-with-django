5f3290413671d1ea77bce0720a81b75055ed3ffb
KuldeepRawat363 <Kuldeep363@users.UI.github.com>
2020-05-09 15:42:02 +0530

    debug

--- library/settings.py
+++ library/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x+$t-1vmxeq=(vpf)55@+%8e(6i())o74z^zfh_sba%3#$(hy3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['kr-library.herokuapp.com']
 
